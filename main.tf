2dbef1ece81be1a5350490663799224cf7d9cbb4
Twinkle <mishrtwinkle1997@gmail.com>
2025-04-05 17:43:00 -0400

    Added tags

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
-  
+
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
@@ -16,8 +16,11 @@ resource "aws_instance" "example" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
   count         = var.core_count
-  
+
   tags = {
-    Name = "8894858_E2_Instance"
+    Name       = "8894858_EC2_Instance_${count.index + 1}"
+    CreatedBy  = "Terraform"
+    ManagedBy  = "CI/CD Lab"
+    Project    = "Lab10"
   }
 }
